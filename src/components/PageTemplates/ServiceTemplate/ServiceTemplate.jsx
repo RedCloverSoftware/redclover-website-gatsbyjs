67ad7cae56ed04469d3b8241ad273f86b1b67d2b
Liam Sherman Parris <lshermanparris@protonmail.com>
2023-01-16 07:42:00 -0400

    Update ServiceTemplate.jsx

--- src/components/PageTemplates/ServiceTemplate/ServiceTemplate.jsx
+++ src/components/PageTemplates/ServiceTemplate/ServiceTemplate.jsx
@@ -13,7 +13,7 @@ const ServiceTemplate = ({ data }) => {
 
   return (
     <Layout>
-      <GradientHeroSection heading_font_size={"44px"}>
+      <GradientHeroSection heading_font_size={"42px"}>
         <PrismicRichText field={page_data.data.page_title.richText} />
       </GradientHeroSection>
 
